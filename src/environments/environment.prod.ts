f2950c5ae640c43b7612e66a15c00010798c9149
Subhendu089 <subhendunayak98@gmail.com>
2024-02-05 12:54:43 +0530

    updated code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
-    production: true, // Change to true for production
+    production: true,
     firebaseConfig: {
-      apiKey: "AIzaSyA9xLaUORSNSl5bexmuktgQApoW9TnCT9g",
-      authDomain: "angularfiredemo-b52e0.firebaseapp.com",
-      databaseURL: "https://angularfiredemo-b52e0-default-rtdb.asia-southeast1.firebasedatabase.app",
-      projectId: "angularfiredemo-b52e0",
-      storageBucket: "angularfiredemo-b52e0.appspot.com",
-      messagingSenderId: "780068906158",
-      appId: "1:780068906158:web:9861b8c31f3bcfa78ef93f"
+      apiKey: '%%FIREBASE_API_KEY%%',
+      authDomain: '%%FIREBASE_AUTH_DOMAIN%%',
+      databaseURL: '%%FIREBASE_DATABASE_URL%%',
+      projectId: '%%FIREBASE_PROJECT_ID%%',
+      storageBucket: '%%FIREBASE_STORAGE_BUCKET%%',
+      messagingSenderId: '%%FIREBASE_MESSAGING_SENDER_ID%%',
+      appId: '%%FIREBASE_APP_ID%%'
     }
   };
+  
