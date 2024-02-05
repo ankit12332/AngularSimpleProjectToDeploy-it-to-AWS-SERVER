12dddbb5e2d265673376b00b5fdad88fbdf31237
Subhendu089 <subhendunayak98@gmail.com>
2024-02-05 11:44:31 +0530

    environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true, // Change to true for production
+    firebaseConfig: {
+      apiKey: "AIzaSyA9xLaUORSNSl5bexmuktgQApoW9TnCT9g",
+      authDomain: "angularfiredemo-b52e0.firebaseapp.com",
+      databaseURL: "https://angularfiredemo-b52e0-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "angularfiredemo-b52e0",
+      storageBucket: "angularfiredemo-b52e0.appspot.com",
+      messagingSenderId: "780068906158",
+      appId: "1:780068906158:web:9861b8c31f3bcfa78ef93f"
+    }
+  };
